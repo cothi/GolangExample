fa4bdd2f9b25a211ec9daa17db3cf91d759f8082
cothi <jiungdev@gmail.com>
2022-08-13 09:07:10 +0900

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cothi/test
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"context"
+
+	"golang.org/x/net/context"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+}
